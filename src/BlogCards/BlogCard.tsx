7f6ce8792003b35ef34d5398ebee671454567f17
Ghanshyam Mali <newmob218@gmail.com>
2024-10-08 11:40:40 +0530

    Added Share feature

--- src/BlogCards/BlogCard.tsx
+++ src/BlogCards/BlogCard.tsx
@@ -8,17 +8,23 @@ import CardActions from "@mui/material/CardActions";
 import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import { Button, Grid } from "@mui/material";
+import { Button, Grid, Menu, MenuItem } from "@mui/material";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShareIcon from "@mui/icons-material/Share";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import { useLoaderData, useNavigate } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/navbar";
-import { Link } from "react-router-dom"; // Correct for routing
+import { Link } from "react-router-dom";
 import { deleteBlogById } from "../Model/BlogCrud";
+
 interface CardData {
   _id: number;
   blog_title: string;
@@ -30,20 +36,34 @@ const commonImagePath = "./logo512.png";
 
 export default function BlogCard() {
   const data: CardData[] = useLoaderData() as CardData[];
- const navigate = useNavigate();
+  const navigate = useNavigate();
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleShareClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   const handleDelete = (_id: number) => {
     console.log("Delete Blog with ID:", _id);
     deleteBlog(_id);
   };
-  async function deleteBlog(_id: Number) {
-    console.log("inside delete");
+
+  async function deleteBlog(_id: number) {
     const data = await deleteBlogById(_id);
-    console.log("delete data", data);
     if (data != null) {
-      window.alert(`project with id ${_id} deleted successfully.....`);
+      window.alert(`Project with id ${_id} deleted successfully.`);
       navigate("/cards");
     } else window.alert("Error during deletion");
   }
+
+  const postUrl = encodeURIComponent(window.location.href);
+  const postTitle = encodeURIComponent("Check out this blog: ");
+
   return (
     <>
       <Navbar />
@@ -88,9 +108,60 @@ export default function BlogCard() {
                 <IconButton aria-label="add to favorites">
                   <FavoriteIcon />
                 </IconButton>
-                <IconButton aria-label="share">
+                <IconButton aria-label="share" onClick={handleShareClick}>
                   <ShareIcon />
                 </IconButton>
+                <Menu
+                  anchorEl={anchorEl}
+                  open={Boolean(anchorEl)}
+                  onClose={handleClose}
+                >
+                  <MenuItem
+                    component="a"
+                    href={`https://www.facebook.com/sharer.php?u=${postUrl}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FacebookIcon sx={{ color: "#3b5998", marginRight: 1 }} />
+                    Facebook
+                  </MenuItem>
+                  <MenuItem
+                    component="a"
+                    href={`https://twitter.com/share?url=${postUrl}&text=${postTitle}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <TwitterIcon sx={{ color: "#1da1f2", marginRight: 1 }} />
+                    Twitter
+                  </MenuItem>
+                  <MenuItem
+                    component="a"
+                    href={`https://pinterest.com/pin/create/bookmarklet/?url=${postUrl}&description=${postTitle}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <PinterestIcon sx={{ color: "#bd081c", marginRight: 1 }} />
+                    Pinterest
+                  </MenuItem>
+                  <MenuItem
+                    component="a"
+                    href={`https://www.linkedin.com/shareArticle?url=${postUrl}&title=${postTitle}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <LinkedInIcon sx={{ color: "#0077b5", marginRight: 1 }} />
+                    LinkedIn
+                  </MenuItem>
+                  <MenuItem
+                    component="a"
+                    href={`https://wa.me/?text=${postTitle} ${postUrl}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <WhatsAppIcon sx={{ color: "#25d366", marginRight: 1 }} />
+                    WhatsApp
+                  </MenuItem>
+                </Menu>
                 <Link
                   to={`/editblog/${card._id}`}
                   style={{ textDecoration: "none" }}
